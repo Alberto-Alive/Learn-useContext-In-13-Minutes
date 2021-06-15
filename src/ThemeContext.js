073c3d77909127c9c62c5b9960a69fbe337adb0b
AlbertoPaulPopescu <63293696+AlbertoPaulPopescu@users.noreply.github.com>
2021-06-15 17:05:53 +0100

    Update ThemeContext.js

--- src/ThemeContext.js
+++ src/ThemeContext.js
@@ -1,34 +1,30 @@
 import React, { useContext, useState } from 'react'
 
-const ThemeContext = React.createContext()
-const ThemeUpdateContext = React.createContext()
+const ThemeContext = React.createContext() // (1) create this ThemeContext that can store a theme
+const ThemeUpdateContext = React.createContext() // (2) create this ThemeUpdateContext that can store a different theme 
 
 export function useTheme() {
-    return useContext(ThemeContext)
-}
+    return useContext(ThemeContext)  
+} // (8) because our colors for the darkTheme is in another file (FunctionContextComponent) we need to send this function there to get the colors
 
 export function useThemeUpdate() {
-    return useContext(ThemeUpdateContext)
-}
+    return useContext(ThemeUpdateContext) 
+} // (9) because our colors for the setDarkTheme (light theme) is in another file (FunctionContextComponent) we need to send this function there to get the colors
 
 export function ThemeProvider({ children}) {
-    const [darkTheme, setDarkTheme] = useState(true)
+    const [darkTheme, setDarkTheme] = useState(true) // (3) create a switch that will be initially set to darkTheme = true 
 
     function toggleTheme() {
-        setDarkTheme(prevDarkTheme => !prevDarkTheme)
+        setDarkTheme(prevDarkTheme => !prevDarkTheme) // (4) create switch logic that will set to darkTheme = flase
     }
 
     return (
-        <ThemeContext.Provider value={darkTheme}>
-            <ThemeUpdateContext.Provider value={toggleTheme}>   
-                {children}
+        <ThemeContext.Provider value={darkTheme}> // (5) set this ThemeContext to darkTheme = true
+            <ThemeUpdateContext.Provider value={toggleTheme}>   // (6) set this ThemeUpdateContext to darkTheme = false
+                {children} // (7) this children will actually be the >> FunctionContextComponent line 15 and 16
             </ThemeUpdateContext.Provider>
         </ThemeContext.Provider>
     )
 }
+//Summary: think of this as the FIRST SQUARE
 
-
-//This is the First Square, this square code spans from line 14 to line 28: this square is supposed to be the logic that wraps around the second square and make everything switch.
-//Line 15 is the useState set to true: it means that darktheme will have the value true initially but when you click on the toggleTheme button you will set this to false.
-//Line 17 is function that takes the second state(setDarkTheme) of the state(darkTheme) and says: use this object 'prevDarkTheme' to get the state of 'darkTheme' and switch it to its opposite (false).
-//Summary: This first square is like a logic skeleton: it decides where the code should sit and how it should act. This first square gets its info from the second square which acts like a database.
