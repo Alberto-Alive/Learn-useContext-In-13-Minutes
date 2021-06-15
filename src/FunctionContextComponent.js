369c98791f1538ceced5697cecdb2c5572771e33
AlbertoPaulPopescu <63293696+AlbertoPaulPopescu@users.noreply.github.com>
2021-06-15 17:21:09 +0100

    Update FunctionContextComponent.js

--- src/FunctionContextComponent.js
+++ src/FunctionContextComponent.js
@@ -2,24 +2,22 @@ import React from 'react'
 import { useTheme, useThemeUpdate } from './ThemeContext'
 
 export default function FunctionContextComponent() {
-    const darkTheme = useTheme()
-    const toggleTheme = useThemeUpdate()
+    const darkTheme = useTheme() // (1) This function useTheme() is here to get the darkTheme colors from themeStyles (line 7)
+    const toggleTheme = useThemeUpdate() // (2) This function useThemeUpdate() is here to be used by the button Toggle Theme (line 15)
     const themeStyles = {
         backgroundColor: darkTheme ? '#333' : '#CCC',
         color: darkTheme ? '#CCC' : '#333',
         padding: '2rem',
         margin: '2rem'
-    }
+    } // (3) themeStyles asks darkTheme if it has the value true, which in turn asks useTheme() to ask the ThemeContext if darkTheme is true
     return (
         <>
-        <button onClick={toggleTheme}> Toggle Theme</button>
+        <button onClick={toggleTheme}> Toggle Theme</button>  
+        // (4) toggleTheme (line 15) asks useThemeUpdate() (line 6) to do its job when button is clicked
         <div style={themeStyles}> Function Theme </div>
+        // (5) this is the element we apply all this madness so we can see it either in a darkish or lightish shade... 
         </>
     )
 }
+// Summary: Think of this as the SECOND SQUARE
 
-//This file is the Second square, this square's code is from line 4 to line 19: this second square is supposed to have what you see on the display i.e buttons, text, colors, etc.
-//Line 5 is used on line 8 and 9
-//Line 6 is used on line 15: it takes the logic from ThemeContext and changes between themes.
-//Line 7 is like a small CSS square where we store the colors for either the dark theme or the light theme.
-//Summary: Second square uses useTheme() and useThemeUpdate(), defines the themeStyles() and displays the toggleTheme button and themeStyles div field.
